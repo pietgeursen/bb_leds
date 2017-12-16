8c1893adefa440ca433a74ea8eba983e9cad4737
Demo User <debian@beaglebone.localdomain>
2017-12-16 13:36:07 +0000

    Refactor

--- leds.c
+++ leds.c
@@ -8,6 +8,7 @@
 #include <sys/ioctl.h>
 
 #define NUM_LEDS 120
+#define SPI_CLK_SPEED 8E6
 
 typedef struct {
   uint8_t brightness; //leading three bits must be ones.
@@ -16,130 +17,82 @@ typedef struct {
   uint8_t red;
 } __attribute__((packed)) led_frame_t;
 
+typedef struct spi_ioc_transfer spi_ioc_transfer_t;
+
 const uint8_t start_frame[4] = {0};
 led_frame_t led_frames[NUM_LEDS] = {0xFF};
 const uint8_t end_frame[8] = {0};
 
+struct spi_ioc_transfer transfers[3];
 
-void transfer_led_frames(int32_t fd){
-  uint8_t rx[sizeof(led_frames)] = {0};
-  uint16_t led_frame_bytes = sizeof(led_frames);
+void init_transfers(spi_ioc_transfer_t * transfers){
+  transfers[0].tx_nbits = 8;
+  transfers[0].delay_usecs = 0;
+  transfers[0].speed_hz = SPI_CLK_SPEED;
+  transfers[0].bits_per_word = 8;
+  transfers[0].len = sizeof(start_frame);
+  transfers[0].tx_buf = (unsigned long)start_frame;
+  transfers[0].cs_change = 0;
 
+  transfers[1].tx_nbits = 8;
+  transfers[1].delay_usecs = 0;
+  transfers[1].speed_hz = SPI_CLK_SPEED;
+  transfers[1].bits_per_word = 8;
+  transfers[1].len = sizeof(led_frames);
+  transfers[1].tx_buf = (unsigned long)led_frames;
+  transfers[2].cs_change = 0;
+
+  transfers[2].tx_nbits = 8;
+  transfers[2].delay_usecs = 0;
+  transfers[2].speed_hz = SPI_CLK_SPEED;
+  transfers[2].bits_per_word = 8;
+  transfers[2].len = sizeof(end_frame);
+  transfers[2].tx_buf = (unsigned long)end_frame;
+  transfers[2].cs_change = 0;
+}
+
+void init_led_frames(led_frame_t * frames){
   for (uint16_t i = 0; i < NUM_LEDS; i++) {
-    led_frames[i].brightness = 0xFF;
-    led_frames[i].red = 0x00;
-    led_frames[i].green = 0x00;
-    led_frames[i].blue = 0x0F;
-  }
-
-  struct spi_ioc_transfer tr = {
-    .tx_buf = (unsigned long)led_frames,
-    .tx_nbits = 8,
-    .rx_buf = (unsigned long)rx,
-    .len = led_frame_bytes,
-    .delay_usecs = 0,
-    .speed_hz = 10000000,
-    .bits_per_word = 8, 
-  };
-
-  int32_t err = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
-  if(err == -1){
-    printf("error led frame %i\n", err);
-  }else {
-    printf("wrote %i bytes\n", err);
+    frames[i].brightness = 0xFF;
+    frames[i].red = 0x00;
+    frames[i].green = 0x00;
+    frames[i].blue = 0x0F;
   }
 }
 
-void transfer_end_frame(int32_t fd){
-  uint8_t rx[sizeof(end_frame)] = {0};
-
-  struct spi_ioc_transfer tr = {
-    .tx_buf = (unsigned long)end_frame,
-    .tx_nbits = 8,
-    .rx_buf = (unsigned long)rx,
-    .len = sizeof(end_frame),
-    .delay_usecs = 0,
-    .speed_hz = 4000000,
-    .bits_per_word = 8, 
-  };
-
-  int32_t err = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
-  if(err == -1){
-    printf("error sending end frame %i\n", err);
-  }else {
-    printf("wrote %i bytes\n", err);
-  }
-
-}
-
-
-void transfer_start_frame(int32_t fd){
-
-  uint8_t rx[sizeof(start_frame)] = {0};
-
-  struct spi_ioc_transfer tr = {
-    .tx_buf = (unsigned long)start_frame,
-    .tx_nbits = 8,
-    .rx_buf = (unsigned long)rx,
-    .len = sizeof(start_frame),
-    .delay_usecs = 0,
-    .speed_hz = 4000000,
-    .bits_per_word = 8, 
-  };
-
-  int32_t err = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
-  if(err == -1){
-    printf("error sending start frame %i\n", err);
-  }else {
-    printf("wrote %i bytes\n", err);
-  }
-}
-
-int32_t main(){
+void init_spi(uint32_t * pFd){
   int32_t err;
   int32_t mode = SPI_MODE_0;
   uint8_t bits = 8;
 
-  uint8_t tx[4] = {0xAA};
-  uint8_t rx[sizeof(tx)] = {0};
+  *pFd = open("/dev/spidev1.0", O_RDWR);
+  printf("fd %i\n", *pFd);
 
-  struct spi_ioc_transfer tr = {
-    .tx_buf = (unsigned long)tx,
-    .tx_nbits = 8,
-    .rx_buf = (unsigned long)rx,
-    .len = sizeof(tx),
-    .delay_usecs = 0,
-    .speed_hz = 4000000,
-    .bits_per_word = 8, 
-  };
-
-
-
-  int32_t fd = open("/dev/spidev1.0", O_RDWR);
-  printf("fd %i\n", fd);
-
-  err = ioctl(fd, SPI_IOC_WR_MODE32, &mode);
+  err = ioctl(*pFd, SPI_IOC_WR_MODE32, &mode);
 	if (err == -1)
     printf("can't set mode bits %i\n", err);
 
-  err = ioctl(fd, SPI_IOC_RD_MODE32, &mode);
+  err = ioctl(*pFd, SPI_IOC_RD_MODE32, &mode);
 	if (err == -1)
     printf("can't set mode bits %i\n", err);
 
-  err = ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
+  err = ioctl(*pFd, SPI_IOC_WR_BITS_PER_WORD, &bits);
 	if (err == -1)
     printf("can't set num bits %i\n", err);
 
-  err = ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &bits);
+  err = ioctl(*pFd, SPI_IOC_RD_BITS_PER_WORD, &bits);
 	if (err == -1)
     printf("can't set num bits %i\n", err);
+}
 
-  //err = write(fd, wr_buf, sizeof(wr_buf));
+int32_t main(){
+  int32_t fd;
+  init_spi(&fd);
+  init_transfers(transfers);
+  init_led_frames(led_frames);
 
-  transfer_start_frame(fd);
-  transfer_led_frames(fd);
-  transfer_end_frame(fd);
+  int32_t err = ioctl(fd, SPI_IOC_MESSAGE(3), transfers);
+  printf("bytes sent: %i\n", err);
 
 	close(fd);
-
 }
